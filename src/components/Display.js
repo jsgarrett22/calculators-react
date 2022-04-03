8fc004d9db7a72944c3c98d2c330be455b892ace
jsgarrett22 <jgarrett35@student.cscc.edu>
2022-04-03 14:10:39 -0400

    Added basic calculation logic of passed prop values to display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export default function Display() {
+const calculateSimpleInterest = (principal, rate, time) => principal * rate * time;
+
+const convertRateToPercentage = rate => rate / 100;
+
+export default function Display(props) {
+    const intEarned = calculateSimpleInterest(props.principal, convertRateToPercentage(props.rate), props.years);
     return (
         <main className="center">
-            <p id="display">Account Value: <span>0</span></p>
+            <p id="display">Account Value: <span>{intEarned}</span></p>
         </main>
     )
 }
