41d11172b34884cc65abe9475780c31f09937120
jsgarrett22 <jgarrett35@student.cscc.edu>
2022-04-04 16:14:23 -0400

    Added conditional rendering to display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -6,9 +6,11 @@ const convertRateToPercentage = rate => rate / 100;
 
 export default function Display(props) {
     const intEarned = calculateSimpleInterest(props.principal, convertRateToPercentage(props.rate), props.years);
+
+    // if the property 'frequency' doesn't exist, display simple interest calculation
     return (
         <main className="center">
-            <p id="display">Account Value: <span>{intEarned}</span></p>
+            <p id="display">Account Value: {props.frequency !== 'undefined' && <span>{intEarned}</span>}</p>
         </main>
     )
 }
